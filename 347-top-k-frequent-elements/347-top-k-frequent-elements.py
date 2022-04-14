403a5a96ca35210009f8f149ea12629093b4b32d
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-04-14 00:52:35 -0500

    Time: 99 ms (95.76%), Space: 18.7 MB (38.31%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res: List[int] = []
+        
+        # create nums freq hash
+        nums_freq: Dict[int, int] = Counter(nums)
+        
+        # max heapify nums freq hash
+        max_heap = [(count, num) for num, count in nums_freq.items()]
+        heapq._heapify_max(max_heap)
+        
+        # pop off the top k
+        for _ in range(k):
+            _, num = heapq._heappop_max(max_heap)
+            res.append(num)
+            
+        return res
+      
