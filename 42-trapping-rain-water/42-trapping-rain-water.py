3853901bbe35f381f0f7a055781673daa633d198
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-04-15 17:25:41 -0500

    Update 42-trapping-rain-water.py

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,29 +1,23 @@
 class Solution:
     def trap(self, heights: List[int]) -> int:
+        if not heights:
+            return 0
+        
+        left, right = 0, len(heights) - 1
+        left_max, right_max = heights[left], heights[right]
+        
         total_water = 0
         
-        # build up the left array
-        left = self.find_max_array(heights)
-        
-        # build up the right array
-        right = self.find_max_array(heights[::-1])[::-1]
-        
-        # iterate through the heights array (indices)
-        for i in range(len(heights)):
-            total_water += min(left[i], right[i]) - heights[i]
-        
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, heights[left])
+                total_water += left_max - heights[left]
+                
+            else:
+                right -= 1
+                right_max = max(right_max, heights[right])
+                total_water += right_max - heights[right]
+                
         return total_water
-    
-    
-    def find_max_array(self, heights: List[int]) -> List[int]:
-        res = []
-        
-        max_ = 0
-        
-        for height in heights:
-            max_ = max(max_, height)
-            res.append(max_)
-            
-        return res
-        
-        
+  
