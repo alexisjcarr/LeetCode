cd622f9937e96a1bcaa9f2c4c5b8f8be9edc95b0
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-04-15 16:14:49 -0500

    Time: 51 ms (77.46%), Space: 15.3 MB (83.08%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -4,17 +4,6 @@ import heapq
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        '''
-        {
-            t: 1,
-            r: 1,
-            e: 2
-        }
-        
-        max_heap = [(count, letter) for letter, count in hash]
-        
-        pop off heap and add to str
-        '''
         res = ""
         freq_hash = Counter(s)
         
@@ -22,7 +11,6 @@ class Solution:
         heapq._heapify_max(max_heap)
         
         while max_heap:
-            # pop off heap
             count, letter = heapq._heappop_max(max_heap)
             
             for _ in range(count):
