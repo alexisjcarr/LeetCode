5b4ec2342f12a50a67a05108a26f07e541d9fe12
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-04-15 23:46:02 -0500

    Update 62-unique-paths.py

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int, memo={}) -> int:
         key = f'{m}, {n}'
@@ -8,4 +11,30 @@ class Solution:
         
         memo[key] = self.uniquePaths(m-1, n, memo) + self.uniquePaths(m, n-1, memo)
         return memo[key]
+
+
+class AltSolution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 and n == 0:
+            return 1
+        
+        count = 0
+        stack = deque()
+        stack.append([0, 0])
+        
+        while stack:
+            r, c = stack.pop()
+            
+            if r == m - 1 and c == n - 1:
+                count += 1
+            
+            # r + 1 in bounds, add to stack
+            if r + 1 < m:
+                stack.append([r + 1, c])
+            
+            # if c + 1 in bounds, add to stack
+            if c + 1 < n:
+                stack.append([r, c + 1])
+            
+        return count
         
