8fcb78798f8f5e1bc4af2cb1704c6e8186d8a7d2
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-04-16 20:15:39 -0500

    Time: 37 ms (66.94%), Space: 13.8 MB (96.87%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -5,11 +5,11 @@ class Solution:
         while num != 0:
             if num % 2 == 0:
                 num /= 2
-                steps += 1
-                
+    
             else:
                 num -= 1
-                steps += 1
+                
+            steps += 1
                 
         return steps
                 
