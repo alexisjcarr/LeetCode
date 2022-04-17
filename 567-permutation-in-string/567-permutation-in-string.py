1fd60800af791f9bbfc0c85f5e3245aec1e90d12
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-04-17 12:04:19 -0500

    Time: 71 ms (91.40%), Space: 14 MB (70.11%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,7 +4,7 @@ from collections import Counter, defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         window_start, freq_hash = 0, defaultdict(int)
-        s1_freq_hash = dict(Counter(s1))
+        s1_freq_hash = Counter(s1)
         
         for window_end in range(len(s2)):
             freq_hash[s2[window_end]] += 1
