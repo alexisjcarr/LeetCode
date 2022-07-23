6c1b05b566e5bf6ab78044bcfc46763f9466ded9
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2022-07-22 22:40:54 -0500

    Time: 210 ms (53.35%), Space: 16.6 MB (84.32%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash = defaultdict(lambda: 0)
+        
+        for num in nums:
+            hash[num] += 1
+            
+        for num in nums:
+            if hash[num] == 1:
+                return num
+            
+        
